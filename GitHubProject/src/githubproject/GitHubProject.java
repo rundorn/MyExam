8c8271f513d55a026c1bed67c8fa3187430d8437
Rundorn Music <Rundorn Music@RundornMusic-PC>
2020-05-13 11:37:52 +0700

    แสดงค่า x

--- GitHubProject/src/githubproject/GitHubProject.java
+++ GitHubProject/src/githubproject/GitHubProject.java
@@ -8,6 +8,7 @@ public class GitHubProject {
     public static void main(String[] args) {
         System.out.println("Hello GitHub");
         int x = 100000;
+        System.out.print(x);
     }
     
 }
